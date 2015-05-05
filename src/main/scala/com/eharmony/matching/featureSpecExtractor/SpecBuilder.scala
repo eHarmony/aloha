579f77e310fd071d6a825a1c556a3c9ef14be2f4
Ryan Deak <rdeak@eharmony.com>
2015-05-05 15:01:12 -0700

    Updated SpecBuilder to work properly from java.  Manually added from* methods from AlohaReadable and delegate to private ReadableByString.  This is a crappy work around to compiler bug SI-8905.  For more info, see https://issues.scala-lang.org/browse/SI-8905

--- src/main/scala/com/eharmony/matching/featureSpecExtractor/SpecBuilder.scala
+++ src/main/scala/com/eharmony/matching/featureSpecExtractor/SpecBuilder.scala
@@ -1,12 +1,16 @@
 package com.eharmony.matching.featureSpecExtractor
 
-import java.{lang => jl, util => ju}
+import java.io.{File, InputStream, Reader}
+import java.net.URL
+import java.{util => ju}
 
 import com.eharmony.matching.aloha.io.{AlohaReadable, ReadableByString}
 import com.eharmony.matching.aloha.semantics.compiled.CompiledSemantics
 import com.eharmony.matching.featureSpecExtractor.SpecType.SpecType
+import org.apache.commons.{vfs, vfs2}
 import spray.json._
 
+import scala.collection.JavaConversions.asScalaBuffer
 import scala.util.{Failure, Try}
 
 
@@ -14,28 +18,48 @@ import scala.util.{Failure, Try}
  * Given a semantics, json specification and an ordered sequence of SpecProducers, ''find the first producer''
  * that applies to creating a Spec from the json specification and use it to instantiate the Spec object.
  * @param semantics a Semantics to be used for creating the Spec.
- * @param specType
- * @param numBits
  * @param producers an ordered sequence of SpecProducers.  These producers form the basis of a
  *                  [[http://en.wikipedia.org/wiki/Chain-of-responsibility_pattern chain of responsibility pattern]].
  *                  Therefore, '''the order is important'''.
+ * @param specType
+ * @param numBits
  * @tparam A the result type produced by reading from one of the readable formats.
  * @tparam B the implementation of Spec[A] used.
  */
-case class SpecBuilder[A, B <: Spec[A]](
+final case class SpecBuilder[A, B <: Spec[A]](
         semantics: CompiledSemantics[A],
+        producers: Seq[SpecProducer[A, B]],
         specType: Option[SpecType] = None,
-        numBits: Option[Int] = None,
-        producers: Seq[SpecProducer[A, B]])
-extends AlohaReadable[Try[B]]
-with ReadableByString[Try[B]] {
+        numBits: Option[Int] = None)
+extends AlohaReadable[Try[B]] {
 
+    /**
+     * Private inner reader delegates to the fromJson method.  All factory (''from*'') functions in the
+     * class delegate to the implementations in the specReadable.  This is because there is a a compiler bug
+     * that causes the return types in the generated method signatures to be Object rather than
+     * ''Try[B]'' when not doing this.  So, we explicitly create those methods.  For more information, see
+     * scala compiler bug:
+     *
+     *   https://issues.scala-lang.org/browse/SI-8905
+     */
+    // TODO: Determine if Serializable is necessary to serialize SpecBuilder.
+    private[this] val specReadable = new ReadableByString[Try[B]] {
+        override def fromString(s: String) = fromJson(s.parseJson)
+    }
 
-    def fromString(s: String) = fromJson(s.parseJson)
+    def fromFile(f: File): Try[B] = specReadable.fromFile(f)
+    def fromUrl(u: URL): Try[B] = specReadable.fromUrl(u)
+    def fromVfs1(foVfs1: vfs.FileObject): Try[B] = specReadable.fromVfs1(foVfs1)
+    def fromVfs2(foVfs2: vfs2.FileObject): Try[B] = specReadable.fromVfs2(foVfs2)
+    def fromResource(r: String): Try[B] = specReadable.fromResource(r)
+    def fromClasspathResource(r: String): Try[B] = specReadable.fromClasspathResource(r)
+    def fromInputStream(is: InputStream): Try[B] = specReadable.fromInputStream(is)
+    def fromReader(r: Reader): Try[B] = specReadable.fromReader(r)
+    def fromString(s: String): Try[B] = specReadable.fromString(s)
 
     def fromJson(json: JsValue): Try[B] = {
-        // create a view so that the map function is applied lazily and then find the first success.
-        // If no producers are successful, return a failure.
+        // Create a view so that the map function is applied lazily and so find attempts to create Spec
+        // instances only until it finds one that works.  If no producers are successful, return a failure.
         val spec = producers.view map { p =>
             for {
                 typedData <- p.parse(json)
@@ -52,10 +76,19 @@ with ReadableByString[Try[B]] {
         Failure { new NoSuchElementException(s"No applicable producer found.  Given ${ps.map(_.name).mkString(", ")}") }
 }
 
-//object SpecBuilder {
-//    @varargs
-//    def apply[A, B <: Spec[A]](
-//            semantics: CompiledSemantics[A],
-//            producers: ju.List[_ <: SpecProducer[A, _ <: B]]): SpecBuilder[A, B] =
-//        SpecBuilder(semantics, None, None, producers.toSeq)
-//}
+object SpecBuilder {
+
+    /**
+     * This is a factory to be used from Java.  Since java.util.List is invariant, we provide a different signature
+     * that accommodates a Java list created by Arrays.asList(...).
+     * @param semantics used to generate the features in the spec.
+     * @param producers a Java List of SpecProducers.
+     * @tparam A type of semantics
+     * @tparam B subtype of Spec objects produced by the SpecBuilder.
+     * @return a new Spec builder.
+     */
+    def apply[A, B <: Spec[A]](
+            semantics: CompiledSemantics[A],
+            producers: ju.List[_ <: SpecProducer[A, _ <: B]]): SpecBuilder[A, B] =
+        SpecBuilder[A, B](semantics, producers.toSeq)
+}
---
+++ src/test/java/com/eharmony/matching/featureSpecExtractor/SpecBuilderJavaTest.java
@@ -0,0 +1,49 @@
+package com.eharmony.matching.featureSpecExtractor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import scala.util.Try;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import static org.junit.Assert.assertNotNull;
+
+import com.eharmony.matching.aloha.semantics.compiled.plugin.csv.CsvLine;
+import com.eharmony.matching.featureSpecExtractor.vw.VwParsingAndChainOfRespTest;
+import com.eharmony.matching.featureSpecExtractor.vw.cb.VwContextualBanditSpecProducer;
+import com.eharmony.matching.featureSpecExtractor.vw.labeled.VwLabelSpecProducer;
+import com.eharmony.matching.featureSpecExtractor.vw.unlabeled.VwSpec;
+
+
+/**
+ * The point of this test is to ensure that calling SpecBuilder from Java works with java Lists.
+ * If the test doesn't throw, then it works.
+ * @author R M Deak
+ */
+@RunWith(BlockJUnit4ClassRunner.class)
+public class SpecBuilderJavaTest {
+    private static String JSON_SPEC_LOC = "com/eharmony/matching/featureSpecExtractor/simpleCbSpec.json";
+
+    @Test
+    public void testCallWithJavaList() {
+
+        @SuppressWarnings("unchecked")
+        final List<? extends SpecProducer<CsvLine, ? extends VwSpec<CsvLine>>> specProducers =
+                Arrays.asList(new VwContextualBanditSpecProducer<CsvLine>(), new VwLabelSpecProducer<CsvLine>());
+
+        final SpecBuilder<CsvLine, VwSpec<CsvLine>> sb = SpecBuilder.apply(
+                VwParsingAndChainOfRespTest.semantics(),
+                specProducers
+        );
+
+        final Try<VwSpec<CsvLine>> vwSpecTry = sb.fromResource(JSON_SPEC_LOC);
+
+        // This would throw if it failed.
+        final VwSpec<CsvLine> csvLineVwSpec = vwSpecTry.get();
+
+        // This shouldn't be necessary but what the heck.
+        assertNotNull(csvLineVwSpec);
+    }
+}
