94d5ccdd819f41e76cfae9b86debb21bdc8ec89a
Ryan Deak <rdeak@eharmony.com>
2015-05-15 12:15:22 -0700

    Made LabelSpec extend Spec.

--- src/main/scala/com/eharmony/matching/featureSpecExtractor/LabelSpec.scala
+++ src/main/scala/com/eharmony/matching/featureSpecExtractor/LabelSpec.scala
@@ -2,6 +2,6 @@ package com.eharmony.matching.featureSpecExtractor
 
 import com.eharmony.matching.aloha.semantics.func.GenAggFunc
 
-trait LabelSpec[-A] {
+trait LabelSpec[-A] extends Spec[A] {
   def label: GenAggFunc[A, String]
 }
