69fe004e3d57cd65ecfe9aa793f6796edc4c4000
Ryan Deak <rdeak@eharmony.com>
2015-08-14 15:39:53 -0700

    Made VW model serializable.

--- aloha-vw-jni/src/test/scala/com/eharmony/aloha/models/vw/jni/VwJniModelTest.scala
+++ aloha-vw-jni/src/test/scala/com/eharmony/aloha/models/vw/jni/VwJniModelTest.scala
@@ -44,7 +44,7 @@ class VwJniModelTest {
     /**
      * This test works locally but fails on jenkins.  Ignore for now.
      */
-    @Ignore @Test def testSerialization(): Unit = {
+    @Test def testSerialization(): Unit = {
         val m = model[Double](typeTestJson)
 
         val baos = new ByteArrayOutputStream()
