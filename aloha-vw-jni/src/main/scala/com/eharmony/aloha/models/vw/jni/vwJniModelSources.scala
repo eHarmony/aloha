33d35ae9b3d85b8f210e5720d9b7ad5e76781c70
Ryan Deak <r.m.deak@gmail.com>
2015-08-24 11:47:22 -0700

    Updated comments and took jmorra's advice on his two comments in #34.

--- aloha-vw-jni/src/main/scala/com/eharmony/aloha/models/vw/jni/vwJniModelSources.scala
+++ aloha-vw-jni/src/main/scala/com/eharmony/aloha/models/vw/jni/vwJniModelSources.scala
@@ -9,21 +9,61 @@ import vw.VW
 
 
 object VwJniModelSource {
+
+    /**
+     * A regular expression to check whether an initial regressor is present in the VW parameters.
+     */
     private[jni] val InitialRegressorPresent = """^(.*\s)?-i.*$"""
 }
 
 /**
- * Needs to be Serializable
+ *
+ * '''NOTE''': ''Must be Serializable.''
  */
 sealed trait VwJniModelSource extends Serializable {
     import VwJniModelSource._
+
+    /**
+     * The VW parameters.
+     * @return
+     */
     def params: String
+
+    /**
+     * This acts as a secondary piece of information on which the temp file names are based.  It doesn't
+     * need to be supplied because a default value is provided: the unixtime in milliseconds.
+     * @return
+     */
     def timeMillis: Long
+
+    /**
+     * The only effectful function to be provided by the subclass.  ''If necessary'', this function copies some
+     * representation of the VW model to the destination file.
+     * @param dest local destination where the VW binary model will be copied.
+     */
     def copyContentToLocalIfNecessary(dest: File): Unit
+
+    /**
+     * Indicates whether to attempt to delete the local file was it is determined it is no longer needed.
+     * If ''copyContentToLocalIfNecessary'' doesn't copy any content to the local disk, then this should
+     * return false.  If it does, this function should return true.
+     * @return
+     */
     def shouldDeleteLocalFile: Boolean
 
+    /**
+     * The local file that will eventually contain the binary VW model.
+     * @param modelId The numeric model identifier.
+     * @return
+     */
     def localFile(modelId: Long): File
 
+    /**
+     * Get the VW model.  If necessary, this will create a local file containing a VW binary model
+     * and will use the initial regressor flag to use it as the starting point to the model.
+     * @param modelId The numeric model identifier.
+     * @return
+     */
     final def vwModel(modelId: Long): VW = {
         val locFile = localFile(modelId)
         val shouldDelete = shouldDeleteLocalFile
@@ -42,6 +82,11 @@ sealed trait VwJniModelSource extends Serializable {
         vw
     }
 
+    /**
+     * Takes the params string and injects the initial regressor parameter.
+     * @param modelId The numeric model identifier.
+     * @return
+     */
     def updatedVwModelParams(modelId: Long): String = {
         val initialRegressorParam = s" -i ${localFile(modelId).getCanonicalPath} "
 
@@ -51,8 +96,12 @@ sealed trait VwJniModelSource extends Serializable {
         initialRegressorParam + params
     }
 
+    /**
+     * Finds the temp directory to be used.
+     * @return
+     */
     def tmpDir() = {
-        val t = File.createTempFile("abc123", "zyx987")
+        val t = File.createTempFile("DOESNT", "MATTER")
         t.deleteOnExit()
         val d = t.getParentFile.getCanonicalFile
         t.delete()
@@ -61,20 +110,35 @@ sealed trait VwJniModelSource extends Serializable {
 
 }
 
+/**
+ * A model source used when the VW binary model is encoded as a base64-encoded string.  The content will always
+ * be copied to a local temp file.
+ * @param b64EncodedBinaryVwModel A base64-encoded String representation of the binary VW model content.
+ * @param params VW model parameters
+ * @param timeMillis timestamp used to help name temp files.
+ */
 final case class Base64EncodedBinaryVwModelSource(
         b64EncodedBinaryVwModel: String,
         params: String = "",
         timeMillis: Long = System.currentTimeMillis()
 ) extends VwJniModelSource {
 
+    /**
+     * @return true
+     */
     def shouldDeleteLocalFile: Boolean = true
 
+    /**
+     * The local temp file.
+     * @param modelId The numeric model identifier.
+     * @return
+     */
     def localFile(modelId: Long): File = new File(tmpDir(), s"aloha.vw.$modelId.$timeMillis.model")
 
     /**
-     * Takes the model ID and base64-encoded binary VW model and creates a temp file with the decoded data
-     * and returns the file handle.
-     * @return File object for the newly created temp file
+     * Decodes the base64-encoded binary VW model and writes it to a temp file at ''dest''.
+     * @param dest where to write the binary VW model.
+     * @return
      */
     def copyContentToLocalIfNecessary(dest: File): Unit = {
         val decoded = Base64.decodeBase64(b64EncodedBinaryVwModel)
@@ -90,18 +154,43 @@ final case class Base64EncodedBinaryVwModelSource(
     }
 }
 
+/**
+ * A model source used when the ''location'' of the VW binary model provided.  If the model is available
+ * locally, it will neither be copied nor deleted.  Otherwise, it'll be copied and deleted when no longer
+ * needed.  The use case for this is copying a large VW model to each node on a cluster and using it over
+ * many batched runs without deleting.  This avoids the network overhead when copying from somewhere (like
+ * HDFS).
+ * @param fsInstance A file system instance pointing to a binary VW model file.  This could be backed by
+ *                   Apache VFS1, VFS2 or a File.  The reason to provide these different abstractions is
+ *                   that different file system abstractions have different plugins that allow different
+ *                   virtual file systems to be used.  For instance, the HDFS plugin used by eH is currently
+ *                   available for VFS1.  An S3 plugin might be on VFS2.  But VFS1 and VFS2 used in concert
+ *                   with each other may through exceptions.  Therefore, we allow the user to specify what
+ *                   to use so that automatic plugin support is available.
+ * @param params VW model parameters
+ * @param timeMillis timestamp used to help name temp files.
+ */
 final case class ExternallyDefinedVwModelSource(
         fsInstance: FsInstance,
         params: String = "",
         timeMillis: Long = System.currentTimeMillis()
 ) extends VwJniModelSource {
-    def shouldDeleteLocalFile: Boolean = !fsInstance.isLocal
+    def shouldDeleteLocalFile: Boolean = !fsInstance.existsLocally
 
+    /**
+     * Points to either a non-temp file that won't be deleted or a temp file that will be deleted.
+     * @param modelId The numeric model identifier.
+     * @return
+     */
     def localFile(modelId: Long): File =
         fsInstance.localFile getOrElse new File(tmpDir(), s"aloha.vw.$modelId.$timeMillis.model")
 
+    /**
+     * Only copies in the file system instance points to a file that is not local.
+     * @param dest local destination where the VW binary model will be copied.
+     */
     def copyContentToLocalIfNecessary(dest: File): Unit = {
-        if (!fsInstance.isLocal) {
+        if (!fsInstance.existsLocally) {
             val is = fsInstance.inputStream
             val os = new FileOutputStream(dest)
             try {
