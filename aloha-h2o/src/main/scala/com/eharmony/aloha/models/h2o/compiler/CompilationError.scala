5ef0c66a30cbc4bf0a9006ae7b04dc63016464ee
Ryan Deak <r.m.deak@gmail.com>
2015-10-31 16:26:10 -0700

    Removing h2o for now.

--- aloha-h2o/src/main/scala/com/eharmony/aloha/models/h2o/compiler/CompilationError.scala
+++
@@ -1,9 +0,0 @@
-package com.eharmony.aloha.models.h2o.compiler
-
-import javax.tools.{JavaFileObject, Diagnostic}
-
-
-/**
- * Created by deak on 9/29/15.
- */
-case class CompilationError(diagnostics: Iterable[Diagnostic[_ <: JavaFileObject]]) extends RuntimeException
