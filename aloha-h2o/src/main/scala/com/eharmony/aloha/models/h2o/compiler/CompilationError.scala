2fc73bb69d1681e2f10a56fa46a2602db19e8a79
Jon Morra <jmorra@eharmony.com>
2016-05-18 10:17:03 -0700

    Adding more informative error message to compilation failures

--- aloha-h2o/src/main/scala/com/eharmony/aloha/models/h2o/compiler/CompilationError.scala
+++ aloha-h2o/src/main/scala/com/eharmony/aloha/models/h2o/compiler/CompilationError.scala
@@ -11,16 +11,8 @@ import scala.io.Source
 case class CompilationError(diagnostics: Iterable[Diagnostic[_ <: JavaFileObject]]) extends RuntimeException {
   override def getMessage: String = {
     val localMessage = diagnostics.map{d =>
-      s"${d.getKind}: at ${d.getLineNumber}L, ${d.getColumnNumber}C: ${d.getMessage(null)}\n  line: ${getSourceLine(d)}"
+      s"${d.getKind}: at ${d.getLineNumber}L, ${d.getColumnNumber}C: ${d.getMessage(null)}"
     }.mkString("\n")
     super.getMessage + "\n" + localMessage
-  }
-
-  private[this] def getSourceLine[A <: JavaFileObject](diagnostic: Diagnostic[A]): String = {
-    val source = Source.fromInputStream(diagnostic.getSource.openInputStream())
-    val lineNum = diagnostic.getLineNumber.toInt
-
-    try source.getLines.slice(lineNum - 1, lineNum).toSeq.headOption.getOrElse("")
-    finally source.close()
   }
 }
