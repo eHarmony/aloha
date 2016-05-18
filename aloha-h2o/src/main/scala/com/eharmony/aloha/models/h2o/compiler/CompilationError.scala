04fdec72f3aba430c878fb8b96045e1c7535e04c
Jon Morra <jmorra@eharmony.com>
2016-05-18 10:26:41 -0700

    Cleaning up error printing

--- aloha-h2o/src/main/scala/com/eharmony/aloha/models/h2o/compiler/CompilationError.scala
+++ aloha-h2o/src/main/scala/com/eharmony/aloha/models/h2o/compiler/CompilationError.scala
@@ -2,16 +2,13 @@ package com.eharmony.aloha.models.h2o.compiler
 
 import javax.tools.{Diagnostic, JavaFileObject}
 
-import scala.io.Source
-
-
 /**
  * Created by deak on 9/29/15.
  */
 case class CompilationError(diagnostics: Iterable[Diagnostic[_ <: JavaFileObject]]) extends RuntimeException {
   override def getMessage: String = {
     val localMessage = diagnostics.map{d =>
-      s"${d.getKind}: at ${d.getLineNumber}L, ${d.getColumnNumber}C: ${d.getMessage(null)}"
+      s"${d.getKind} at ${d.getLineNumber}L, ${d.getColumnNumber}C: ${d.getMessage(null)}"
     }.mkString("\n")
     super.getMessage + "\n" + localMessage
   }
