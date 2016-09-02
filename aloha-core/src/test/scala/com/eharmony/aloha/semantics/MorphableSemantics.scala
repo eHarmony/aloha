39fbc122b392a5692bac4d5b33992fd9e56277c1
Ryan Deak <r.m.deak@gmail.com>
2016-09-02 16:05:03 -0700

    Updated documentation and change from a context bound to an implicit argument for RefInfo.

--- aloha-core/src/test/scala/com/eharmony/aloha/semantics/MorphableSemantics.scala
+++ aloha-core/src/test/scala/com/eharmony/aloha/semantics/MorphableSemantics.scala
@@ -5,13 +5,49 @@ import com.eharmony.aloha.reflect.RefInfo
 import scala.language.higherKinds
 
 /**
-  * A version of [[Semantics]] that can morph into other instance of the same structure but
+  * A version of [[Semantics]] that can morph into other instances of the same structure but
   * with a different type parameter.
-  * @tparam M a type constructor the gives
-  * @tparam A
+  *
+  * An example:
+  *
+  * {{{
+  * case class X[A]() extends Semantics[A] with MorphableSemantics[X, A] {
+  *   def semantics: X[A] = this
+  *   def morph[B: RefInfo]: Option[X[B]] = Some(X[B]())
+  * }
+  * }}}
+  *
+  * Notice for class `X`, that the `M` type constructor is `X` and the `A` is the same for
+  * the second type parameter of [[MorphableSemantics]] as for the type parameter in [[Semantics]].
+  *
+  * This can be use like the following:
+  *
+  * {{{
+  * import com.eharmony.aloha.reflect.RefInfo
+  *
+  * val xF = X[Float]
+  * val riD = implicitly[RefInfo[Double]]
+  * val xDo: Option[X[Double]] = xF.morph[Double]
+  * val xDo1: Option[X[Double]] = xF.morph(riD)
+  * }}}
+  *
+  * or use it generically:
+  *
+  * {{{
+  * def morph[M[_] <: Semantics[_], A, B: RefInfo](m: MorphableSemantics[M, A]): Option[Semantics[B]] = {
+  *   m.morph[B].map(ms => ms.semantics)
+  * }
+  *
+  * val sDO: Option[Semantics[Double]] = morph(xF)
+  * }}}
+  *
+  * @tparam M a type constructor providing the structure of the [[Semantics]].  This will most
+  *           likely always be the concrete subclass of [[Semantics]] that implements
+  *           [[MorphableSemantics]].
+  * @tparam A the type parameter to which `M` is applied for this instance of [[Semantics]].
+  *           By the self-type, also the type parameter for the [[Semantics]].
   * @author deaktator
   */
-// TODO: Finish documentation...
 trait MorphableSemantics[M[_] <: Semantics[_], A] { self: Semantics[A] =>
 
   /**
@@ -50,11 +86,13 @@ trait MorphableSemantics[M[_] <: Semantics[_], A] { self: Semantics[A] =>
     * type parameter) but with a different second parameter.  This new instance must also be
     * a `Semantics[B]` because of the type bound on `M` and the self-type of
     * [[MorphableSemantics]].  It can be the case that a specific implementation of
-    * [[MorphableSemantics]] doesn't allow for all `B`, so it can return a `None`.
+    * [[MorphableSemantics]] doesn't allow for all `B`, in which case a `None` can be returned.
+    * @param ri reflection information that may be necessary to determine whether to create
+    *           the [[MorphableSemantics]] that was requested.
     * @tparam B input type for the new [[MorphableSemantics]] instance that might be created.
     *           A [[MorphableSemantics]] instance may choose not allow morphing to all `B`.
     *           In that case, a `None` will be returned.
     * @return
     */
-  def morph[B: RefInfo]: Option[MorphableSemantics[M, B]]
+  def morph[B](implicit ri: RefInfo[B]): Option[MorphableSemantics[M, B]]
 }
