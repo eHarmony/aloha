df1e5e01d756a596d378c98ff4857d4b8c3a8342
Ryan Deak <r.m.deak@gmail.com>
2016-09-17 14:35:44 -0700

    updated protobuf install script

--- travis-install-protobuf.sh
+++ travis-install-protobuf.sh
@@ -4,7 +4,14 @@ set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/archive/v2.4.1.zip
   unzip v2.4.1.zip
-  cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  cd protobuf-2.4.1
+  mkdir gtest-download && cd gtest-download
+  wget https://github.com/google/googletest/archive/release-1.3.0.zip
+  unzip release-1.3.0.zip
+  cd ..
+  mv gtest-download/googletest-release-1.3.0 gtest
+  ./autogen.sh
+  ./configure --prefix=$HOME/protobuf && make && make install
   cd -
 else
   echo "Using cached directory."
