1098dce0aab1940301e3a0dd6033d9bc222c5eaf
Ryan Deak <r.m.deak@gmail.com>
2016-11-27 10:50:08 -0700

    simpler and more general find command.

--- travis-mv-filtered.sh
+++ travis-mv-filtered.sh
@@ -4,7 +4,7 @@ set -e
 
 function mv_filtered() {
   for v in 10 11; do
-    for d in $(find */target/scala-2.${v}/filtered -maxdepth 3 -type d -name "filtered_resources"); do
+    for d in $(find */target/scala-2.${v}/filtered -mindepth 3 -maxdepth 3 -type d); do
       PROJECT=$(echo $d | grep -Po '^[^/]*')
       if [ $(echo $d | grep '/test/') ]; then
         CLASS_PREFIX='test-'
