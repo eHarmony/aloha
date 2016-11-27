9b6a1563c0261997b04171447ec1992f7d47513f
Ryan Deak <r.m.deak@gmail.com>
2016-11-27 08:08:54 -0700

    Add temporary travis script.

--- travis-mv-filtered.sh
+++ travis-mv-filtered.sh
@@ -1,11 +1,7 @@
-Need to create an SBT that does the following and is called after
-`editsource:edit`.  This plugin needs to be included in the projects that
-need it.  Additionally, this plugin should be called as part of the release
-process so that com/eharmony/aloha/version.properties has a **release** version
-and not a **SNAPSHOT** version.
+#!/bin/sh
 
-```zsh
-# To be run in project root directory.
+set -e
+
 function mv_filtered() {
   for v in 10 11; do
     for d in $(find */target/scala-2.${v}/filtered -name "*" -d 3); do
@@ -22,4 +18,3 @@ function mv_filtered() {
     done
   done
 }
-```
