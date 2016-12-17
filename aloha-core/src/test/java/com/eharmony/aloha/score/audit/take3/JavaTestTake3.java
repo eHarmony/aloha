3b81ac7c17551518c8bc1e2e0f9b68527c93964d
Ryan Deak <r.m.deak@gmail.com>
2016-12-17 13:35:57 -0700

    updated tests, Added EitherAuditor

--- aloha-core/src/test/java/com/eharmony/aloha/score/audit/take3/JavaTestTake3.java
+++ aloha-core/src/test/java/com/eharmony/aloha/score/audit/take3/JavaTestTake3.java
@@ -1,6 +1,7 @@
 package com.eharmony.aloha.score.audit.take3;
 
 import com.eharmony.aloha.id.ModelId;
+import com.eharmony.aloha.id.ModelIdentity;
 import deaktator.reflect.runtime.manifest.ManifestParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import scala.Option;
 import scala.reflect.Manifest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by ryan on 12/16/16.
@@ -21,21 +23,82 @@ public class JavaTestTake3 {
         final OptionAuditor<Integer> cmAud = new OptionAuditor<>(refInfoInt);
 
         final ConstantModel<TypeCtor, Integer, Object, Option<Integer>> cModel =
-                new ConstantModel<>(ModelId.empty(), 1, cmAud);
-
-        final Model<Object, Option<Integer>> submodel = cModel;
+            new ConstantModel<>(ModelId.empty(), 1, cmAud);
 
         final Float constant = 2f;
         final Manifest<Float> refInfoFloat = manifest(Float.class.getCanonicalName());
         final OptionAuditor<Float> aud = new OptionAuditor<>(refInfoFloat);
 
         final HierarchicalConstantModel<TypeCtor, Object, Float, Object, Option<Float>> hcm =
-            new HierarchicalConstantModel<>(ModelId.empty(), constant, submodel, aud);
+            new HierarchicalConstantModel<>(ModelId.empty(), constant, cModel, aud);
 
         final Model<Object, Option<Float>> m = hcm;
 
         assertEquals(Option.apply(constant), m.apply(null));
     }
+
+    @Test
+    public void test2() {
+        final Manifest<Integer> refInfoInt = manifest(Integer.class.getCanonicalName());
+        final OptionAuditor<Integer> cmAud = new OptionAuditor<>(refInfoInt);
+
+        final Model<Object, Option<Integer>> cModel =
+            new ConstantModel<>(ModelId.empty(), 1, cmAud);
+
+        final Float constant = 2f;
+        final Manifest<Float> refInfoFloat = manifest(Float.class.getCanonicalName());
+        final OptionAuditor<Float> aud = new OptionAuditor<>(refInfoFloat);
+
+        final Model<Object, Option<Float>> hcm =
+            new HierarchicalConstantModel<>(ModelId.empty(), constant, cModel, aud);
+
+        assertEquals(Option.apply(constant), hcm.apply(null));
+    }
+
+    @Test
+    public void test3() {
+        final Manifest<Integer> refInfoInt = manifest(Integer.class.getCanonicalName());
+        final EitherAuditor<Integer> cmAud = new EitherAuditor<>(refInfoInt);
+
+        final ConstantModel<TypeCtor, Integer, Object, EitherAuditor.Result<ModelIdentity, Integer>> cModel =
+                new ConstantModel<>(ModelId.empty(), 1, cmAud);
+
+        final Float constant = 2f;
+        final Manifest<Float> refInfoFloat = manifest(Float.class.getCanonicalName());
+        final OptionAuditor<Float> aud = new OptionAuditor<>(refInfoFloat);
+
+        final Model<Object, Option<Float>> hcm =
+                new HierarchicalConstantModel<>(ModelId.empty(), constant, cModel, aud);
+
+        assertEquals(Option.apply(constant), hcm.apply(null));
+    }
+
+    @Test
+    public void test4() {
+        final Manifest<Integer> refInfoInt = manifest(Integer.class.getCanonicalName());
+        final EitherAuditor<Integer> cmAud = new EitherAuditor<>(refInfoInt);
+
+        final ConstantModel<TypeCtor, Integer, Object, EitherAuditor.Result<ModelIdentity, Integer>> cModel =
+                new ConstantModel<>(new ModelId(1, "one"), 1, cmAud);
+
+        final Float constant = 2f;
+        final Manifest<Float> refInfoFloat = manifest(Float.class.getCanonicalName());
+        final EitherAuditor<Float> aud = new EitherAuditor<>(refInfoFloat);
+
+        final HierarchicalConstantModel<TypeCtor, Object, Float, Object, EitherAuditor.Result<ModelIdentity, Float>> hcm =
+                new HierarchicalConstantModel<>(new ModelId(2, "two"), constant, cModel, aud);
+
+        final EitherAuditor.Result<ModelIdentity, Float> res = hcm.apply(null);
+        if (res instanceof EitherAuditor.Success) {
+            EitherAuditor.Success<ModelIdentity, Float> success = (EitherAuditor.Success<ModelIdentity, Float>) res;
+            assertEquals(1, success.subValues().size());
+            assertEquals(2f, success.valueToAudit(), 0);
+        }
+        else {
+            fail();
+        }
+    }
+
 
     @SuppressWarnings("unchecked")
     private static <A> Manifest<A> manifest(final String strRep) {
---
+++ aloha-core/src/test/scala/com/eharmony/aloha/score/audit/take3/EitherAuditor.scala
@@ -0,0 +1,29 @@
+package com.eharmony.aloha.score.audit.take3
+
+import com.eharmony.aloha.id.ModelIdentity
+import com.eharmony.aloha.reflect.RefInfo
+import com.eharmony.aloha.score.audit.take3.EitherAuditor.{Failure, Result, Success}
+
+/**
+  * Created by ryan on 12/17/16.
+  */
+case class EitherAuditor[A](implicit val refInfo: RefInfo[A]) extends TypedAuditor[ModelIdentity, TypeCtor.Aux[({type L[+X] = Result[ModelIdentity, X]})#L], A, Result[ModelIdentity, A]] {
+  /**
+    * Change the type of an auditor to one of the same shape, but with a different type
+    * parameter `C` instead of `A`.
+    *
+    * @tparam C The new type being audited.
+    * @return
+    */
+  override private[aloha] def changeType[C: RefInfo] = EitherAuditor[C]
+  override private[aloha] def failure[S](key: ModelIdentity, errorMsgs: => Seq[String], missingVarNames: => Set[String], subValues: Seq[Result[ModelIdentity, S]]): Failure[ModelIdentity] =
+    Failure(key, errorMsgs, missingVarNames, subValues)
+  override private[aloha] def success[S](key: ModelIdentity, valueToAudit: A, missingVarNames: => Set[String], subValues: Seq[Result[ModelIdentity, S]], prob: => Option[Double]): Success[ModelIdentity, A] =
+    Success(key, valueToAudit, missingVarNames, subValues, prob)
+}
+
+object EitherAuditor {
+  sealed trait Result[K, +V]
+  case class Failure[K](key: K, errorMsgs: Seq[String], missingVarNames: Set[String], subValues: Seq[Result[K, Any]]) extends Result[K, Nothing]
+  case class Success[K, +V](key: K, valueToAudit: V, missingVarNames: Set[String], subValues: Seq[Result[K, Any]], prob: Option[Double]) extends Result[K, V]
+}
