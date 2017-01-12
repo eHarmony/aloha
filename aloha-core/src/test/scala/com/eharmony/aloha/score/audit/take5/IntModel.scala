4cdda36ad655c17c30a56000a18c916deb3f7008
Ryan Deak <r.m.deak@gmail.com>
2017-01-12 14:42:27 -0700

    Updated IntModel to remove existential types from constructor.  Leave these in the java factory method.

--- aloha-core/src/test/scala/com/eharmony/aloha/score/audit/take5/IntModel.scala
+++ aloha-core/src/test/scala/com/eharmony/aloha/score/audit/take5/IntModel.scala
@@ -14,7 +14,7 @@ import com.eharmony.aloha.id.ModelIdentity
 case class IntModel[U, +B <: U](
     modelId: ModelIdentity,
     value: Int,
-    auditor: Auditor[U, _ >: Int <: Int, B]
+    auditor: Auditor[U, Int, B]
 ) extends AuditedModel[U, Int, Any, B] {
   override def apply(v1: Any): B = auditor.success(modelId, value, Set.empty, Nil, None)
 }
@@ -26,42 +26,19 @@ object IntModel {
     * The problem with a model having a natural type that is a subtype of `AnyVal` is that
     * these types resolve to `Object` when the byte code is emitted.  To get around this,
     * we provide a Java-oriented factory method where the type parameter `N` is a supertype
-    * and subtype of `Float`, and becomes an unbound type parameter in the emitted code.
+    * and subtype of `Int`, and becomes an unbound type parameter in the emitted code.
     * Ultimately, this resolves to the following Java code:
     *
     * `
-    * public static <U, N, B extends U> FloatModel<U, B>
-    * createFromJava(ModelIdentity modelId, float value, Auditor<U, N, B> auditor)
-    * `
-    *
-    * If instead of including this convenience method, we chose to change the definition of
-    * the `auditor` parameter in the [[IntModel]] constructor to:
-    *
-    * {{{
-    * auditor: Auditor[U, _ >: Float, B]
-    * }}}
-    *
-    * and attempted to instantiate the model via:
-    *
-    * `
-    * final FloatModel<Option<?>, Option<Float>> model =
-    *   new FloatModel<>(modelId, expected, auditor);
-    * `
-    *
-    * We'd see the following error:
-    *
-    * `
-    * Error:(58, 60) java: cannot infer type arguments for com.eharmony.aloha.score.audit.take5.FloatModel<>
-    *   reason: cannot infer type-variable(s) U,B
-    *     (argument mismatch; com.eharmony.aloha.score.audit.take5.OptionAuditor<java.lang.Float>
-    *       cannot be converted to com.eharmony.aloha.score.audit.take5.Auditor<U,? super java.lang.Object,B>)
+    * public static <U, N, B extends U> IntModel<U, B>
+    *   createFromJava(ModelIdentity modelId, int value, Auditor<U, N, B> auditor)
     * `
     *
     * @param modelId a model identifier
     * @param value a value returned by the model.
-    * @param auditor an auditor capable of auditing a `Float` and turning it into a `B`.
+    * @param auditor an auditor capable of auditing a `Int` and turning it into a `B`.
     * @tparam U The upper bound pof the audited type hierarchy.
-    * @tparam N a super type of the model's natural type: `Float`.
+    * @tparam N a super type of the model's natural type: `Int`.
     * @tparam B The output type.
     * @return
     */
