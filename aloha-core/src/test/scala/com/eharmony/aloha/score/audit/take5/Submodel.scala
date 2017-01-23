743e90ccbbf2b4b4c24ab0c3056d5a52d30cff98
Ryan Deak <r.m.deak@gmail.com>
2017-01-23 09:31:54 -0700

    decoupled model and submodel

--- aloha-core/src/test/scala/com/eharmony/aloha/score/audit/take5/Submodel.scala
+++ aloha-core/src/test/scala/com/eharmony/aloha/score/audit/take5/Submodel.scala
@@ -3,6 +3,6 @@ package com.eharmony.aloha.score.audit.take5
 /**
   * Created by ryan on 1/18/17.
   */
-trait Submodel[+N, -A, +B] extends Model[A, B] {
+trait Submodel[+N, -A, +B] {
   def subvalue(a: A): Subvalue[B, N]
 }
