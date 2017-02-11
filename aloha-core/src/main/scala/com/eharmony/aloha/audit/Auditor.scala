e655a89f90c9c82d70bc071a0a675a50c3b78cfe
Ryan Deak <r.m.deak@gmail.com>
2017-02-10 21:39:41 -0700

    Comment on Auditor.

--- aloha-core/src/main/scala/com/eharmony/aloha/audit/Auditor.scala
+++ aloha-core/src/main/scala/com/eharmony/aloha/audit/Auditor.scala
@@ -3,7 +3,14 @@ package com.eharmony.aloha.audit
 import com.eharmony.aloha.id.ModelIdentity
 
 /**
+  * API for recursive auditing.
+  *
   * Created by ryan on 1/11/17.
+  *
+  * @tparam U The upper type bound for items being audited.
+  * @tparam N The "''natural''" output type of a given model.
+  * @tparam B The output type of the Auditor.  This is the same as the output type of a
+  *           [[com.eharmony.aloha.models.Model]].
   */
 trait Auditor[U, -N, +B <: U] {
   def failure(modelId: ModelIdentity,
