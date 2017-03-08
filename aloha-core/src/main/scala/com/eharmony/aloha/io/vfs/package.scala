e6ec14853a3e83c270ce475bd7ead30f09da763c
Ryan Deak <r.m.deak@gmail.com>
2017-03-08 14:35:03 -0700

    updated vfs1 and vfs2 file object var names to make it more readable.

--- aloha-core/src/main/scala/com/eharmony/aloha/io/vfs/package.scala
+++ aloha-core/src/main/scala/com/eharmony/aloha/io/vfs/package.scala
@@ -105,8 +105,8 @@ object Vfs {
         }
     }
 
-    implicit def apacheVfs1ToAloha(fo: vfs1.FileObject): Vfs1 = Vfs1(fo)
-    implicit def apacheVfs2ToAloha(fo: vfs2.FileObject): Vfs2 = Vfs2(fo)
+    implicit def apacheVfs1ToAloha(vfs1Fo: vfs1.FileObject): Vfs1 = Vfs1(fo)
+    implicit def apacheVfs2ToAloha(vfs2Fo: vfs2.FileObject): Vfs2 = Vfs2(fo)
     implicit def javaFileToAloha(f: java.io.File): File = File(f)
 }
 
