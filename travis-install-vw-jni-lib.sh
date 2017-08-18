03b76df73833b77511a8d75e61f699bab23a9758
Ryan Deak <r.m.deak@gmail.com>
2017-08-18 16:45:28 -0700

    Travis vw install update 0009

---
+++ travis-install-vw-jni-lib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# This is the git hash for the JNI 8.4.1 release
+# For more info, see: https://github.com/JohnLangford/vowpal_wabbit/tree/master/java
+#
+VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
+
+# Where to put VW library.  See .travis.yml.
+JAVA_LIB_PATH_DIR=$HOME/lib
+
+git clone https://github.com/JohnLangford/vowpal_wabbit.git
+cd vowpal_wabbit
+git fetch
+git checkout $VW_RELEASE_HASH
+make java
+mkdir -p $JAVA_LIB_PATH_DIR
+cp java/target/libvw_jni.so $JAVA_LIB_PATH_DIR
+cd -
