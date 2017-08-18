b2e1df9c05eab01d3f1626b6998572a9900cede6
Ryan Deak <r.m.deak@gmail.com>
2017-08-18 15:40:38 -0700

    Travis vw install update 0002

--- travis-install-vw.sh
+++ travis-install-vw.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 set -e
 
-# git clone https://github.com/JohnLangford/vowpal_wabbit.git
-# cd vowpal_wabbit
-# make java
-# echo "put the JNI lib in one of these:"
-for F in $((echo -e 'public class Main{public static void main(String[] args){ System.out.println(System.getProperty("java.library.path"));}}' > Main.java && javac Main.java && java Main) | tr : '\n'); do
-  echo -e "\t$F"
-done
+# This is the git hash for the JNI 8.4.1 release
+# For more info, see: https://github.com/JohnLangford/vowpal_wabbit/tree/master/java
+#
+VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
+
+git clone https://github.com/JohnLangford/vowpal_wabbit.git
+cd vowpal_wabbit
+git fetch
+git checkout 10bd09ab06f59291e04ad7805e88fd3e693b7159
+make java
+echo "Contents in java/target"
+ls -halF java/target
+
+# VW's libvw_jni.so needs to go in one of the following:
+#   /usr/java/packages/lib/amd64
+#   /usr/lib64
+#   /lib64
+#   /lib
+#   /usr/lib
+#
+# based on the following code:
+#
+#   for F in $((echo -e 'public class Main{public static void main(String[] args){System.out.println(System.getProperty("java.library.path"));}}' > Main.java && javac Main.java && java Main) | tr : '\n'); do
+#     echo -e "\t$F"
+#   done
+#
+
 exit 1
 cd -
