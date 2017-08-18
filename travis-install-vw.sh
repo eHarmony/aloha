8e82f4364785023b8d93a38077b67b5ac8faac77
Ryan Deak <r.m.deak@gmail.com>
2017-08-18 15:34:27 -0700

    Travis vw install update 0001

--- travis-install-vw.sh
+++ travis-install-vw.sh
@@ -5,7 +5,7 @@ set -e
 # cd vowpal_wabbit
 # make java
 # echo "put the JNI lib in one of these:"
-for F in $((echo -e 'public class Main{public static void main(String[] args){ System.out.println(System.getProperty("java.library.path"));}}' > Main.java && javac Main.java) | tr : '\n'); do
+for F in $((echo -e 'public class Main{public static void main(String[] args){ System.out.println(System.getProperty("java.library.path"));}}' > Main.java && javac Main.java && java Main) | tr : '\n'); do
   echo -e "\t$F"
 done
 exit 1
