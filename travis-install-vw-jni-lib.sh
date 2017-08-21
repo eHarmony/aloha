05677a48d4b9fc0b671cc334581183d1e8427d79
Ryan Deak <r.m.deak@gmail.com>
2017-08-21 13:11:16 -0700

    travis headaches 003

--- travis-install-vw-jni-lib.sh
+++ travis-install-vw-jni-lib.sh
@@ -5,19 +5,29 @@ set -e
 # For more info, see: https://github.com/JohnLangford/vowpal_wabbit/tree/master/java
 #
 VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
+EXPECTED_VW_LIB_SHA256=NONHEX_DUMMY
+
 
 # Where to put VW library.  See .travis.yml.
 VW_LIB_DIR=$HOME/vw
+VW_JNI_LIB = $VW_LIB_DIR/libvw_jni.so
+VW_LIB_SHA256=$(openssl dgst -sha256 $VW_JNI_LIB 2>/dev/null | sed 's/..* //g')
 
-git clone https://github.com/JohnLangford/vowpal_wabbit.git
-# git fetch https://github.com/JohnLangford/vowpal_wabbit.git $VW_RELEASE_HASH
 
-# git fetch --tags --progress https://github.com/JohnLangford/vowpal_wabbit.git +refs/heads/*:refs/remotes/origin/*
+if [[ "$VW_LIB_SHA256" != "$EXPECTED_VW_LIB_SHA256" ]]; then
+  yellow "VW JNI library hash '$VW_LIB_SHA256' doesn't match expected: '$EXPECTED_VW_LIB_SHA256'."
+  yellow "Compiling VW JNI lib."
 
-cd vowpal_wabbit
-git fetch
-git checkout $VW_RELEASE_HASH
-make java
-mkdir -p $VW_LIB_DIR
-cp java/target/libvw_jni.so $VW_LIB_DIR
-cd -
+  git glone https://github.com/JohnLangford/vowpal_wabbit.git
+  cd vowpal_wabbit
+  git fetch
+  git checkout $VW_RELEASE_HASH
+  make java
+  mkdir -p $VW_LIB_DIR
+  cp java/target/libvw_jni.so $VW_LIB_DIR
+  NEW_VW_LIB_SHA256=$(openssl dgst -sha256 $VW_JNI_LIB 2>/dev/null | sed 's/..* //g')
+
+  green "$VW_JNI_LIB exists with SHA 256 hash '$NEW_VW_LIB_SHA256'"
+
+  cd -
+fi
