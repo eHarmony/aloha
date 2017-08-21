ca733cfa5da29087f95cce4976e1b8c5ae286160
Ryan Deak <r.m.deak@gmail.com>
2017-08-21 10:28:34 -0700

    Updated h2o version.  Removed unused import in h2o test.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val log4jVersion = "1.2.17"
 
   val vwJniVersion = "8.4.1"
-  val h2oVersion = "3.8.2.3"
+  val h2oVersion = "3.10.3.2"
   val guavaVersion = "16.0.1"
   val avroVersion = "1.8.1"
 
