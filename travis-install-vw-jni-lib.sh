814e58059eec66b0404e983820e241acec545d70
Ryan Deak <r.m.deak@gmail.com>
2017-08-21 10:54:07 -0700

    only check out the VW stuff we need.

--- travis-install-vw-jni-lib.sh
+++ travis-install-vw-jni-lib.sh
@@ -9,10 +9,11 @@ VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
 # Where to put VW library.  See .travis.yml.
 JAVA_LIB_PATH_DIR=$HOME/lib
 
-git clone https://github.com/JohnLangford/vowpal_wabbit.git
+# git clone https://github.com/JohnLangford/vowpal_wabbit.git
+git fetch https://github.com/JohnLangford/vowpal_wabbit.git 0a071603d87e0b89738599c160583a19a6d95545
 cd vowpal_wabbit
-git fetch
-git checkout $VW_RELEASE_HASH
+#git fetch
+#git checkout $VW_RELEASE_HASH
 make java
 mkdir -p $JAVA_LIB_PATH_DIR
 cp java/target/libvw_jni.so $JAVA_LIB_PATH_DIR
