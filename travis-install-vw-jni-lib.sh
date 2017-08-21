456bf2c3b6e5b785cf9cd3ab9ab1d9b274805917
Ryan Deak <r.m.deak@gmail.com>
2017-08-21 15:02:03 -0700

    travis headaches 015: commenting vw jni install script.

--- travis-install-vw-jni-lib.sh
+++ travis-install-vw-jni-lib.sh
@@ -5,14 +5,19 @@ set -e
 # For more info, see: https://github.com/JohnLangford/vowpal_wabbit/tree/master/java
 #
 VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
+
+# The expected SHA-256 hash of the libvw_jni.so for the associated VW_RELEASE_HASH
+# when built by TravisCI.
 EXPECTED_VW_LIB_SHA256=476714cc030dc95640dc3ffaa2fa0c92fcca9e37f2edb7695e72af01d989537d
+
+# The concurrency level of the VW build.  VW typically uses all of the cores but this might be
+# bad on TravisCI since it's a shared environment.
 BUILD_CONCURRENCY=4
 
-# Where to put VW library.  See .travis.yml.
+# Where to put VW library.  This should be the same as in .travis.yml.
 VW_LIB_DIR=$HOME/vw
 VW_JNI_LIB=$VW_LIB_DIR/libvw_jni.so
 VW_LIB_SHA256=$(openssl dgst -sha256 $VW_JNI_LIB 2>/dev/null | sed 's/..* //g')
-
 
 
 if [[ "$VW_LIB_SHA256" != "$EXPECTED_VW_LIB_SHA256" ]]; then
@@ -24,7 +29,7 @@ if [[ "$VW_LIB_SHA256" != "$EXPECTED_VW_LIB_SHA256" ]]; then
   git fetch
   git checkout $VW_RELEASE_HASH
 
-  # Modify make file to use less cores because the travis server is shared.
+  # Modify make file to use less cores because the TravisCI servers are shared.
   mv Makefile Makefile.orig
   cat Makefile.orig | sed "s/-j  *\$(NPROCS)/-j $BUILD_CONCURRENCY/g" > Makefile
 
