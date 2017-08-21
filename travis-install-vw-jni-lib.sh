4011f929220b2a483034466273a2097d4edd59e3
Ryan Deak <r.m.deak@gmail.com>
2017-08-21 12:39:01 -0700

    travis headaches 002

--- travis-install-vw-jni-lib.sh
+++ travis-install-vw-jni-lib.sh
@@ -7,7 +7,7 @@ set -e
 VW_RELEASE_HASH=10bd09ab06f59291e04ad7805e88fd3e693b7159
 
 # Where to put VW library.  See .travis.yml.
-JAVA_LIB_PATH_DIR=$HOME/lib
+VW_LIB_DIR=$HOME/vw
 
 git clone https://github.com/JohnLangford/vowpal_wabbit.git
 # git fetch https://github.com/JohnLangford/vowpal_wabbit.git $VW_RELEASE_HASH
@@ -18,6 +18,6 @@ cd vowpal_wabbit
 git fetch
 git checkout $VW_RELEASE_HASH
 make java
-mkdir -p $JAVA_LIB_PATH_DIR
-cp java/target/libvw_jni.so $JAVA_LIB_PATH_DIR
+mkdir -p $VW_LIB_DIR
+cp java/target/libvw_jni.so $VW_LIB_DIR
 cd -
