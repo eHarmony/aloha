597e1fe722815289355840fb7db9df5a942d85a9
Ryan Deak <r.m.deak@gmail.com>
2017-08-28 20:39:46 -0700

    Travis publish site docs attempt 010. Removed dianostics.

--- travis-decrypt-keys.sh
+++ travis-decrypt-keys.sh
@@ -3,6 +3,3 @@
 openssl aes-256-cbc -K $encrypted_33aac24de5f8_key -iv $encrypted_33aac24de5f8_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
 chmod 600 travis-deploy-key;
 cp travis-deploy-key ~/.ssh/id_rsa;
-
-# green assumes sourcing travis-bash-fns.sh
-green "Key SHA-512   $(openssl dgst -sha512 ~/.ssh/id_rsa | sed 's/.*  *//g')";
