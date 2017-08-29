27a45e4d79c2205a126dabaea9c0c12d7209adf7
Ryan Deak <r.m.deak@gmail.com>
2017-08-29 09:16:19 -0700

    Travis publish site docs attempt 014. Stupid green.

--- travis-publish-microsite.sh
+++ travis-publish-microsite.sh
@@ -5,6 +5,4 @@ git config --global user.email "opensource@eharmony.com"
 git config --global user.name "eHarmony"
 git config --global push.default simple
 
-# green assumes sourcing travis-bash-fns.sh
-green "publishing docs"
 sbt docs/publishMicrosite
