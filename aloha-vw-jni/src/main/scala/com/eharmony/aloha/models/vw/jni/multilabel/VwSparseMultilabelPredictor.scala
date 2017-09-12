6f98ce225c37c95d2f18637241415b3afafbfbb2
Ryan Deak <r.m.deak@gmail.com>
2017-09-12 10:51:14 -0700

    Added comments to VwSparseMultilabelPredictor.

--- aloha-vw-jni/src/main/scala/com/eharmony/aloha/models/vw/jni/multilabel/VwSparseMultilabelPredictor.scala
+++ aloha-vw-jni/src/main/scala/com/eharmony/aloha/models/vw/jni/multilabel/VwSparseMultilabelPredictor.scala
@@ -13,9 +13,15 @@ import scala.collection.{immutable => sci}
 import scala.util.Try
 
 /**
+  *
   * Created by ryan.deak on 9/8/17.
-  * @param modelSource a specification of a location for the underlying VW model that
-  *                    will be materialized in this class.
+  * @param modelSource a specification for the underlying ''Cost Sensitive One Against All''
+  *                    VW model with ''label dependent features''.  VW flag
+  *                    `--csoaa_ldf mc` is expected. For more information, see the
+  *                    [[https://github.com/JohnLangford/vowpal_wabbit/wiki/Cost-Sensitive-One-Against-All-(csoaa)-multi-class-example VW CSOAA wiki page]].
+  *                    Also see the ''Cost-Sensitive Multiclass Classification'' section of
+  *                    Hal Daume's [[https://www.umiacs.umd.edu/%7Ehal/tmp/multiclassVW.html On Multiclass Classification in VW]]
+  *                    page.  This model specification will be materialized in this class.
   * @param params VW parameters.
   * @param defaultNs The list of indices into the `features` sequence that does not have
   *                  an exist in any value of the `namespaces` map.
@@ -49,6 +55,16 @@ extends SparseMultiLabelPredictor[K]
     require(vwModel != null)
   }
 
+  /**
+    * Given the input, form a VW example, and delegate to the underlying ''CSOAA LDF'' VW model.
+    * @param features  (non-label dependent) features shared across all labels.
+    * @param labels labels for which the VW learner should produce predictions.
+    * @param indices the indices `labels` into the sequence of all labels encountered
+    *                during training.
+    * @param labelDependentFeatures Any label dependent features.  This is not yet utilized and
+    *                               is currently ignored.
+    * @return a Map from label to prediction.
+    */
   override def apply(
       features: IndexedSeq[Sparse],
       labels: sci.IndexedSeq[K],
@@ -67,6 +83,17 @@ extends SparseMultiLabelPredictor[K]
 object VwSparseMultilabelPredictor {
   private val ClassNS = "Y"
 
+  /**
+    * Produce a multi-line input to be consumed by the underlying ''CSOAA LDF'' VW model.
+    * @param features (non-label dependent) features shared across all labels.
+    * @param indices the indices `labels` into the sequence of all labels encountered
+    *                during training.
+    * @param defaultNs the indices into `features` that should be placed in VW's default
+    *                  namespace.
+    * @param namespaces the indices into `features` that should be associated with each
+    *                   namespace.
+    * @return an array to be passed directly to an underlying `VWActionScoresLearner`.
+    */
   private[multilabel] def multiLabelClassifierInput(
       features: IndexedSeq[Sparse],
       indices: sci.IndexedSeq[Int],
@@ -96,11 +123,17 @@ object VwSparseMultilabelPredictor {
     x
   }
 
+  /**
+    * Produce the output given VW's output, `pred`, and the labels provided to the `apply` function.
+    * @param pred predictions returned by the underlying VW ''CSOAA LDF'' model.
+    * @param labels the labels provided to the `apply` function.  This determines which predictions
+    *               should be produced.
+    * @tparam K The label or class type.
+    * @return a map of predictions from label to prediction.
+    */
   private[multilabel] def produceOutput[K](pred: ActionScores, labels: sci.IndexedSeq[K]): Map[K, Double] = {
-
-    // The last two action IDs in the action scores array are the dummy actions.
     (for {
-      as    <- pred.getActionScores // if as.getAction < indices.size if we need to deal with dummy classes.
+      as    <- pred.getActionScores
       label  = labels(as.getAction)
       pred   = modifiedLogistic(as.getScore)
     } yield label -> pred)(collection.breakOut)
@@ -108,8 +141,9 @@ object VwSparseMultilabelPredictor {
 
   /**
     * A modified logistic function where the sign of the exponent is opposite the usual
-    * definition.  Since CSOAA in VW employs costs, it changes the sign of the normal
-    * logistic function so the definition becomes `1 / (1 + exp(x))`.
+    * definition.  Since CSOAA in VW employs costs, it returns the negative logits which
+    * changes the sign of the normal logistic function so the definition becomes
+    * `1 / (1 + exp(x))`.
     *
     * @param x an input produced by a VW CSOAA prediction.
     * @return a probability.
