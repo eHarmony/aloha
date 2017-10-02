944672d056f2f3e4d01ab63f59cae61864b596b8
Ryan Deak <r.m.deak@gmail.com>
2017-10-02 13:57:12 -0700

    Bumped stack to 4M for tut until 0.5.5+ can be used.

--- travis-publish-microsite.sh
+++ travis-publish-microsite.sh
@@ -5,4 +5,4 @@ git config --global user.email "opensource@eharmony.com"
 git config --global user.name "eHarmony"
 git config --global push.default simple
 
-sbt -J-Xss3m docs/publishMicrosite
+sbt -J-Xss4m docs/publishMicrosite
